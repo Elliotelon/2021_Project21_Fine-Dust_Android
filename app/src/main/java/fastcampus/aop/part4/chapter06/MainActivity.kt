cb24af989329ceaab963efa15ccc9586efe0f8d3
kimminkyu <kmk50009@naver.com>
2021-05-22 01:47:22 +0900

    Fine Dust 실시간 대기정보 보여주기

--- app/src/main/java/fastcampus/aop/part4/chapter06/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part4/chapter06/MainActivity.kt
@@ -5,12 +5,16 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.CancellationTokenSource
 import fastcampus.aop.part4.chapter06.data.Repository
+import fastcampus.aop.part4.chapter06.data.models.airquality.Grade
+import fastcampus.aop.part4.chapter06.data.models.airquality.MeasuredValue
+import fastcampus.aop.part4.chapter06.data.models.monitoringstation.MonitoringStation
 import fastcampus.aop.part4.chapter06.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -29,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        bindViews()
         initVariables()
         requestLocationPermissions()
 
@@ -53,6 +58,12 @@ class MainActivity : AppCompatActivity() {
             finish()
         } else {
 
+            fetchAirQualityData()
+        }
+    }
+
+    private fun bindViews() {
+        binding.refresh.setOnRefreshListener {
             fetchAirQualityData()
         }
     }
@@ -85,19 +96,75 @@ class MainActivity : AppCompatActivity() {
                 cancellationTokenSource!!.token
             ).addOnSuccessListener { location ->
                 scope.launch {
-                    val monitoringStation =
-                       Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
+                    binding.errorDescriptionTextView.visibility = View.GONE
+                    try {
+                        val monitoringStation =
+                            Repository.getNearbyMonitoringStation(
+                                location.latitude,
+                                location.longitude
+                            )
+                        val measuredValue =
+                            Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
 
-                    val measuredValue =
-                        Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
-
-                    binding.textView.text = measuredValue.toString()
+                        displayAirQualityData(monitoringStation, measuredValue!!)
+                    } catch (exception: Exception) {
+                        exception.printStackTrace()
+                        binding.errorDescriptionTextView.visibility = View.VISIBLE
+                        binding.contentsLayout.alpha = 0F
+                    } finally {
+                        binding.progressBar.visibility = View.GONE
+                        binding.refresh.isRefreshing = false
+                    }
                 }
+            }
+    }
 
+    @SuppressLint("SetTextI18n")
+    fun displayAirQualityData(monitoringStation: MonitoringStation, measuredValue: MeasuredValue) {
 
+        binding.contentsLayout.animate()
+            .alpha(1F)
+            .start()
+
+        binding.measuringStationNameTextView.text = monitoringStation.stationName
+        binding.measuringStationAddressTextView.text = "측정소 위치: ${monitoringStation.addr}"
+
+        (measuredValue.khaiGrade ?: Grade.UNKNOWN).let { grade ->
+            binding.root.setBackgroundResource(grade.colorResId)
+            binding.totalGradeLabelTextView.text = grade.label
+            binding.totalGradleEmojiTextView.text = grade.emoji
+        }
+
+        with(measuredValue) {
+            binding.fineDustInformationTextView.text =
+                "미세먼지: $pm10Value ㎍/㎥ ${(pm10Grade ?: Grade.UNKNOWN).emoji}"
+            binding.ultraFineDustInformationTextView.text =
+                "초미세먼지: $pm25Value ㎍/㎥ ${(pm25Grade ?: Grade.UNKNOWN).emoji}"
+
+            with(binding.so2Item) {
+                labelTextView.text = "아황산가스"
+                gradeTextView.text = (so2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$so2Value ppm"
             }
 
+            with(binding.coItem) {
+                labelTextView.text = "일산화탄소"
+                gradeTextView.text = (coGrade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$coValue ppm"
+            }
 
+            with(binding.o3Item) {
+                labelTextView.text = "오존"
+                gradeTextView.text = (o3Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$o3Value ppm"
+            }
+
+            with(binding.no2Item) {
+                labelTextView.text = "이산화질소"
+                gradeTextView.text = (no2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$no2Value ppm"
+            }
+        }
     }
 
 
