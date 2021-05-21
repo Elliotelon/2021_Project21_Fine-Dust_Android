5db53c8b94871cd3d85e1d3a1b874955f8ee75cc
kimminkyu <kmk50009@naver.com>
2021-05-21 19:57:22 +0900

    Fine Dust 사용자 위치정보 불러오기

--- app/src/main/java/fastcampus/aop/part4/chapter06/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part4/chapter06/MainActivity.kt
@@ -1,11 +1,87 @@
 package fastcampus.aop.part4.chapter06
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import fastcampus.aop.part4.chapter06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? =null
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initVariables()
+        requestLocationPermissions()
+
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+                requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if(!locationPermissionGranted) {
+            finish()
+        } else {
+            //fetchData
+            cancellationTokenSource = CancellationTokenSource()
+
+            fusedLocationProviderClient
+                    .getCurrentLocation(
+                            LocationRequest.PRIORITY_HIGH_ACCURACY,
+                            cancellationTokenSource!!.token
+                    ).addOnSuccessListener { location ->
+                        binding.textView.text = "${location.latitude}, ${location.longitude}"
+
+
+                    }
+
+
+        }
+    }
+
+    private fun initVariables() {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    private fun requestLocationPermissions(){
+        ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
+    }
+
 }
